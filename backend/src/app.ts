570cba0d63b70a8da6b39e9ffa2be4715338ab17
GusCabraal <gustavocabral97@hotmail.com>
2022-11-26 18:47:31 -0300

    feat: adição do cors e axios

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import handleErrors from './middlewares/handleErrors';
 import { userRoutes, movieRoutes, loginRoutes } from './routes';
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/login', loginRoutes);
 app.use('/users', userRoutes);
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
+import httpRequest from '../axios/config';
 import { useHistory } from 'react-router-dom';
 
 const MIN_PASSWORD_LENGTH = 6;
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const history = useHistory();
   
-  const handleClick = (event) => {
+  
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    history.push('movies');
+    try {
+      const { data: { token } } = await httpRequest.post('/login', { email, password });
+      console.log(token);
+      localStorage.setItem('token', token);
+      history.push('/movies');
+    } catch (error) {
+      setError(error.message);
+    }
   };
   return (
     <div>
       <main>
-        <form onSubmit={ handleClick }>
+        <form onSubmit={ handleSubmit }>
           <input
             type="email"
             value={ email }
@@ -28,6 +38,7 @@ function Login() {
             onChange={ (e) => setPassword(e.target.value) }
             placeholder="Digite sua senha"
           />
+          {error && <p>{error}</p>}
           <button
             type="submit"
             disabled={ !(email.match(/\S+@\S+\.\S+/) && password.length > MIN_PASSWORD_LENGTH) }
