30e804a038c9930fcad00913b662f13cec89c100
GusCabraal <gustavocabral97@hotmail.com>
2022-12-08 22:15:30 -0300

    teste com token no axios

--- frontend/src/axios/config.ts
+++ frontend/src/axios/config.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
+let token = '';
+const user = localStorage.getItem("user") as string;
+if(user){
+  token = JSON.parse(user).token;
+}
+
 const httpRequest = axios.create({
   baseURL: 'http://localhost:3001',
   headers: {
     common: {
-      Authorization: JSON.parse(localStorage.getItem("user") as string).token || '',
+      Authorization: token || '',
     },
   },
 });
